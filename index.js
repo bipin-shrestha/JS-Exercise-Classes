41a19e94e140eb297f67402e127cd019abb6552c
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-10-08 21:49:09 -0700

    Task 4 is completed

--- index.js
+++ index.js
@@ -110,11 +110,6 @@ car.fill(20);
 //car.drive(75);
 console.log(car.drive(500));
 
-
-
-
-
-
 /*
   TASK 3
     - Write a Lambdasian class.
@@ -128,8 +123,21 @@ console.log(car.drive(500));
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+    constructor(data) {
+        this.name = data.name;
+        this.age = data.age;
+        this.location = data.location;
+    }
+    speak() {
+        return "Hello my name is " + this.name + " , I am from " + this.location + ".";
+    }
 }
+
+const doublu = new Lambdasian({ name: "Doublu", age: 32, location: "Sacramento" });
+console.log(doublu.speak());
+
+
+
 
 /*
   TASK 4
@@ -145,8 +153,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+    constructor(info) {
+        super(info);
+        this.specialty = info.specialty;
+        this.favLanguage = info.favLanguage;
+        this.catchPhrase = info.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
 }
 
 /*
@@ -154,15 +173,32 @@ class Instructor {
     - Write a Student class extending Lambdasian.
     - Its constructor takes a single argument -  an object with the following keys:
         + All the keys used to initialize instances of Lambdasian.
-        + `previousBackground` i.e. what the Student used to do before Lambda School
-        + `className` i.e. CS132
-        + `favSubjects`. i.e. an array of the student's favorite subjects ['HTML', 'CSS', 'JS']
+        + `
+            previousBackground ` i.e. what the Student used to do before Lambda School
+        + `
+            className ` i.e. CS132
+        + `
+            favSubjects `. i.e. an array of the student's favorite subjects ['HTML', 'CSS', 'JS']
     - The constructor calls the parent constructor passing to it what it needs.
-    - The constructor should also initialize `previousBackground`, `className` and `favSubjects` properties on the instance.
+    - The constructor should also initialize `
+            previousBackground `, `
+            className ` and `
+            favSubjects ` properties on the instance.
     - Student instances have the following methods:
-        + `listSubjects` a method that returns all of the student's favSubjects in a single string: `Loving HTML, CSS, JS!`.
-        + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
-        + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
+        + `
+            listSubjects ` a method that returns all of the student's favSubjects in a single string: `
+            Loving HTML,
+            CSS,
+            JS!`.
+        + `
+            PRAssignment ` a method that receives a subject as an argument and returns `
+            student.name has submitted a PR
+            for { subject }
+            `
+        + `
+            sprintChallenge ` similar to PRAssignment but returns `
+            student.name has begun sprint challenge on { subject }
+            `
 */
 class Student {
 
@@ -173,26 +209,35 @@ class Student {
     - Write a ProjectManager class extending Instructor.
     - Its constructor takes a single argument - an object with the following keys:
         + All the keys used to initialize instances of Instructor.
-        + `gradClassName`: i.e. CS1
-        + `favInstructor`: i.e. Sean
+        + `
+            gradClassName `: i.e. CS1
+        + `
+            favInstructor `: i.e. Sean
     - Its constructor calls the parent constructor passing to it what it needs.
-    - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
+    - The constructor should also initialize `
+            gradClassName ` and `
+            favInstructor ` properties on the instance.
     - ProjectManager instances have the following methods:
-        + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
-        + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
-*/
-class ProjectManager {
+        + `
+            standUp ` a method that takes in a slack channel and returns ` { name }
+            announces to { channel },
+            @channel standy times!`
+        + `
+            debugsCode ` a method that takes in a student object and a subject and returns ` { name }
+            debugs { student.name }
+            's code on {subject}` * /
+            class ProjectManager {
 
-}
+            }
 
-/*
-  STRETCH PROBLEM (no tests!)
-    - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-    - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-    - Add a graduate method to a student.
-      + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-      + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
-*/
+            /*
+              STRETCH PROBLEM (no tests!)
+                - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+                - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+                - Add a graduate method to a student.
+                  + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+                  + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+            */
 
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
